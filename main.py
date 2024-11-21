8618cd53a7d32470cac7609d8f39f2ec2b5edfb4
emeline-lgd <122470795+emeline-lgd@users.noreply.github.com>
2024-11-21 09:44:02 +0000

    05-palindrome rendu emeline lagarde

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
+"""
+Dans cet exercice on cherche a savoir si une chaine de caractère
+est un palindrome ou non.
+avec les fonction  isprimeispalindrome(p) et main()
+"""
 #### Fonction secondaire
 
 
 def ispalindrome(p):
 
     # votre code ici
-    
+
+    """
+    Cette fonction nous permet de retourner un booléen 
+    si la chaine de caratere mis en argument "p" est un palindrome (True)  si non (False)
+    """
+    p = p.lower()
+    p = p.translate(str.maketrans("éèêëàâäîïôöùûüç","eeeeaaaiioouuuc"," !:;,?./'()-"))
+    if p == p[::-1]:
+        return True
     return False
 
 #### Fonction principale
 
 
 def main():
+    """
+    main() est la fonction principale qui print si s est un palindrome
+    """
 
     # vos appels à la fonction secondaire ici
 
